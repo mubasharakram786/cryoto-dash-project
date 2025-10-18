e8c84246947f263484e6ebe24803a61fadf85077
mubasharakram786 <mubashar1418@gmail.com>
2025-10-18 13:46:55 +0500

    Feat: Fetching and displaying coins

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useState,useEffect} from 'react'
+import CoinCard from './components/CoinCard'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [coins,setCoins] = useState([])
+  const [loading,setLoading] = useState(true)
+  const [error,setError] = useState(null)
 
-  return (
+  useEffect(()=>{
+    const fetchCoins = async() =>{
+      try {
+        const res = await fetch(`${import.meta.env.VITE_API_URL}&order=market_cap_desc&per_page=10&page=1&sparkline=false`);
+        if(!res.ok){
+          return new Error('Failed to fetch records')
+        }
+        let coins = await res.json()
+        setCoins(coins);
+        setLoading(false)
+      } catch (error) {
+        setLoading(false)
+        setError(error.message)
+      }
+    }
+    fetchCoins()
+  },[])
+
+  return(
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h1>🚀 Crypto Dash</h1>
+  {loading && <p>Loading</p>}
+  {error && <div className='error'>{error}</div>}
+    {!loading && !error && (
+      <main className='grid'>
+          {coins.map((coin)=>(
+            <CoinCard coin={coin} key={coin.id}/>
+          ))}
+      </main>
+    )}
     </>
+
   )
 }
 
