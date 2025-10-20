2ac85ebece259d8e5de5472608ffc992a0dcef8a
mubasharakram786 <mubashar1418@gmail.com>
2025-10-20 14:19:22 +0500

    Feat: Sorting login

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import {useState,useEffect} from 'react'
 import CoinCard from './components/CoinCard'
 import LimitSelect from './components/LimitSelect'
 import FilterInput from './components/FilterInput'
+import SortSelector from './components/SortSelector'
 
 const App = () => {
   const [coins,setCoins] = useState([])
@@ -9,6 +10,7 @@ const App = () => {
   const [error,setError] = useState(null)
   const[limit,setLimit] = useState(10)
   const [filter,setFilter] = useState('')
+  const [sortBy,setSortBy] = useState('market_cap_desc')
   useEffect(()=>{
     const fetchCoins = async() =>{
       try {
@@ -29,6 +31,21 @@ const App = () => {
     const filteredCoins = coins.filter((coin)=>{
       return coin.name.toLowerCase().includes(filter.toLocaleLowerCase()) ||
       coin.symbol.toLowerCase().includes(filter.toLocaleLowerCase())
+    }).slice().sort((a,b)=>{
+      switch(sortBy){
+        case 'market_cap_asc':
+          return a.market_cap - b.market_cap
+        case 'market_cap_desc':
+          return b.market_cap - a.market_cap
+        case 'price_asc':
+          return a.current_price - b.current_price
+        case 'price_desc':
+          return b.current_price - a.current_price  
+        case 'change_desc':
+          return b.price_change_percentage_24h - a.price_change_percentage_24h
+        case 'change_asc':
+          return a.price_change_percentage_24h - b.price_change_percentage_24h
+      }
     })
   return(
     <>
@@ -36,8 +53,10 @@ const App = () => {
   {loading && <p>Loading</p>}
   {error && <div className='error'>{error}</div>}
   <div className="top-controls">
+
        <FilterInput filter={filter} onFilterChange={setFilter} /> 
       <LimitSelect limit={limit} onLimitChange={setLimit} />
+      <SortSelector sortBy={sortBy} onSortChange={setSortBy}/>
   </div>
     {!loading && !error && (
       <main className='grid'>
