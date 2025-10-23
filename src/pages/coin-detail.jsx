9d05bb7771b704d61a8f6d3b4437d6247d0dafd8
mubasharakram786 <mubashar1418@gmail.com>
2025-10-23 18:29:29 +0500

    API call and log the coin details

--- src/pages/coin-detail.jsx
+++ src/pages/coin-detail.jsx
@@ -1,17 +1,32 @@
 import { useState,useEffect } from "react"
 import { useParams } from "react-router-dom"
+
 const CoinDetailPage = () =>{
     const [coin , setCoin] = useState(null)
     const [loading,setLoading] = useState(true)
     const [error,setError] = useState(null)
     const {id} = useParams()
-    // useEffect(()=>{
-    //     const fetchCoinDetails=async()=>{
-    //         const response = await fetch()
-    //     }
-    // },[])
+    useEffect(()=>{
+        const fetchCoinDetails=async()=>{
+            try {
+                const response = await fetch(`${import.meta.env.VITE_COIN_API_URL}/${id}`);
+                if(!response.ok){
+                    throw Error("Failed to fetch coin details")
+                }
+                const coinDetails = await response.json();
+                console.log(coinDetails);
+                setLoading(false);
+                setCoin(coinDetails)
+                
+            } catch (error) {
+                setLoading(false)
+                setError(error.message)
+            }
+        }
+        fetchCoinDetails()
+    },[])
     return(
-        <div>Detail Page: {id} </div>
+        <div>Detail Page: {JSON.stringify(coin)} </div>
     )
 }
 
